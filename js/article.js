d92fe661892318ee7dddd7458ba14d789168bd09
Your Name <you@example.com>
2025-07-19 16:23:50 +0800

    ..

--- js/article.js
+++ js/article.js
@@ -61,6 +61,10 @@ for (var i = 0; i < Life_Record.length; i++) {
 
 var Product_Documentation = [
 	{
+		title : "产品的组合变型式与实际商业应用",
+		url : "/article.html?Product_Documentation/产品的组合变型式与实际商业应用.md"
+	},
+	{
 		title : "末世质量值得信赖",
 		url : "/article.html?Product_Documentation/末世质量值得信赖.md"
 	},
